00d6a85e5a82b4d85a261483a86f7fcee84ef7ed
GumulaRonewa <44128309+GumulaRonewa@users.noreply.github.com>
2020-11-17 12:13:09 +0200

    Update Register.js

--- src/pages/login/Register.js
+++ src/pages/login/Register.js
@@ -118,7 +118,7 @@ class Register extends Component {
               localStorage.setItem('name',user.name);
 
                        console.log(res);
-                       window.open('https://longamoney.groundrabbit.co.za/')
+                       window.open('https://longamoney.groundrabbit.co.za/', "_self"))
          
             });
     } else {
